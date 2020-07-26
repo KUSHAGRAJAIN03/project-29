ee3a633c80dc9035c5b251d7a0418eca40cf6897
KUSHAGRAJAIN03 <66012878+KUSHAGRAJAIN03@users.noreply.github.com>
2020-07-26 12:36:11 +0530

    Add files via upload

---
+++ Sling_shot.js
@@ -0,0 +1,27 @@
+class Sling_shot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 5
+        }
+        this.pointB=pointB;
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        if (this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var B = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y,B.x,B.y);
+    }
+}
+    fly(){
+        this.chain.bodyA=null;
+    }
+}
+
+         
